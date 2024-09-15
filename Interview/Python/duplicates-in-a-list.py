5f843814879383f35cd0d71dbc9116bd795cc824
Danish A <42688495+danisha138@users.noreply.github.com>
2024-09-15 10:08:58 +0530

    Update duplicates-in-a-list.py

--- Interview/Python/duplicates-in-a-list.py
+++ Interview/Python/duplicates-in-a-list.py
@@ -5,17 +5,15 @@ res = []
 for x in a:
     if a.count(x) > 1 and x not in res:
         print(str(x) + " is appeared "+str(a.count(x)) + " times")  
-        res.append(x)
-      
+        res.append(x)  
 print(res)
 
-
+##################################################
 #Remove the duplicates 
 a = [1, 2, 3, 2, 1, 5, 6, 5, 5, 5]
 print('provided list' +str(test_list))
 set_temp=list(set(test_list))
 print(set_temp)
-
 
 res=[]
 [res.append(i) for i in a if i not in res]
