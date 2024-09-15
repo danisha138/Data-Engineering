11d5d1447a016e4ac924c88d51ad67174e62a1d4
Danish A <42688495+danisha138@users.noreply.github.com>
2024-09-15 10:15:29 +0530

    Create chech-if-string-contains-only-digits.py

---
+++ Interview/Python/chech-if-string-contains-only-digit.py
@@ -0,0 +1,9 @@
+def contains_only_digits(s):
+  for char in s:
+    if char < '0' or char > '9'     # Check if char is not between '0' and '9'
+    False
+  True
+  
+s="1235d"
+
+print(f"{s} contains only digits {contains_only_digits(s)}")
